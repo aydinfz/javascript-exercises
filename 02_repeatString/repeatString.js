37eae7543cf252a823a59e9202f4ac9bb802db07
Zehra <aydin.fzehra@gmail.com>
2022-07-17 16:14:39 +0300

    complete 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  if (typeof string == "string" && typeof num == "number") {
+    if (num < 0) {
+      return "ERROR";
+    } else {
+      return string.repeat(num);
+    }
+  }
 };
 
 // Do not edit below this line
