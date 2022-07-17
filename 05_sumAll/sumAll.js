d96acedbc20c81524965f6ca3641de080a27ccab
Zehra <aydin.fzehra@gmail.com>
2022-07-17 23:56:27 +0300

    complete 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,13 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+if (typeof x != "number" || typeof y != "number") {return "ERROR"}
+if (x<0 || y<0) {return "ERROR"}
+if (y<x){
+    return x*(x+1)/2 - y*(y-1)/2
+} else if (y>x) {
+    return y*(y+1)/2 - x*(x-1)/2 
+} else{
+    return "ERROR"
+}
 
 };
 
